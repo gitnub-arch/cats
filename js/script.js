4cd065258ef511a8a37f80d22b1eddc76d8a8860
Kostenko Oleg <ok609035@gmail.com>
2024-06-24 20:38:36 +0300

    added js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
-const list = document.querySelector("button");
-const dropdown = document.querySelector("#drop");
+// const list = document.querySelector("button");
+// const dropdown = document.querySelector("#drop");
 
-list.addEventListener("click", () => {
-  dropdown.classList.toggle("active");
-});
+// list.addEventListener("click", () => {
+//   dropdown.classList.toggle("active");
+// });
 
-const btn = document.querySelector("#btn");
-const popup = document.querySelector("#popup");
+// const btn = document.querySelector("#btn");
+// const popup = document.querySelector("#popup");
 
-btn.addEventListener("click", () => {
-  popup.classList.toggle("active");
-});
+// btn.addEventListener("click", () => {
+//   popup.classList.toggle("active");
+// });
 
-const headerButton = document.getElementById("header__button");
+// const headerButton = document.getElementById("header__button");
 
-headerButton.addEventListener("click", function () {
-  this.classList.toggle("hamburger--open");
-});
+// headerButton.addEventListener("click", function () {
+//   this.classList.toggle("hamburger--open");
+// });
+
+
+
+
+const navItems = [...document.getElementsByClassName('nav__item')];
+
+navItems.forEach(item => {
+  item.addEventListener('click', () => {
+    navItems.forEach(oldItem => oldItem.classList.remove('active'))
+
+    item.classList.add('active')
+  })
+})
